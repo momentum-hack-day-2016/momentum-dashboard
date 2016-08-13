9564ca28c23c875fbdc29d370adb0dfa11fd845a
Colm <crdoconnor+momentum@gmail.com>
2016-08-13 15:26:55 +0100

    Implemented sign up form functionality

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseNotFound, HttpResponseRedirect
 
 from forms import MomentumSignupForm
 
 
 ## MOMENTUM SPECIFIC CODE BELOW
 
-def signup(request):
+def momentum_signup(request):
     if request.method == 'POST':
         form = MomentumSignupForm(request.POST)
         if form.is_valid():
-            return HttpResponseRedirect("/dashboard")
+            return HttpResponseRedirect("/dash")
     else:
         form = MomentumSignupForm(
             initial={
@@ -21,3 +22,7 @@ def signup(request):
         )
 
     return render_to_response("signup-momentum.html", locals(), context_instance=RequestContext(request))
+
+
+def dashboard(request):
+    return render_to_response("dashboard-momentum.html", locals(), context_instance=RequestContext(request))
